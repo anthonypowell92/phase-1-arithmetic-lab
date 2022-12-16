ca8020c3f5fc0440aa213dbff64a5dda8a49ea21
AnthonyPowell <anthonydewpow@gmail.com>
2022-12-16 16:35:13 -0600

    commit tree 7a8d407ab269fed00849848f1b82ad436ac5ec6d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()) + 1;
+console.log(random);
+
+let num3 = 32
+let num4 = 8
+const mod = num3 / num4
+
+let max = Math.max(5, 7, 8, 20);
+console.log(max);
